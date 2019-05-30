7da5ef5cd5f97bdd37fbec4aebbb81a7cb472837
Kyle Chesney <kyle.2493@gmail.com>
2019-05-30 08:57:07 -0400

    relocate entry of username and staff_id to control-center

--- plugins/user_attributes.php
+++ plugins/user_attributes.php
@@ -4,4 +4,4 @@ use REDCapEntity\EntityList;
 
 $list = new EntityList('user_attributes', $module);
 $list->setOperations(['create', 'update', 'delete'])
-    ->render('project');
+    ->render('control_center');
