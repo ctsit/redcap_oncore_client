70a9ba70d73ee1080b4f61fcd9ca683f761003a2
Philip Chase <pbc@ufl.edu>
2020-03-04 16:36:31 -0500

    Update test data and script for multi-valued race

--- examples/make_summary_accrual_test_data.R
+++ examples/make_summary_accrual_test_data.R
@@ -16,6 +16,13 @@ gender <- sample(c("M", "F", "B", NA), records, replace = T)
 race <- sample(c("01", "03", "04", "05", "06", "07", "99", NA), records, replace = T)
 ethnicity <- sample(c("1", "2", "3", "9", NA), records, replace = T)
 
+race___01 <- sample(c(1,0,0,0,0), records, replace = T)
+race___03 <- sample(c(1,0,0,0,0), records, replace = T)
+race___04 <- sample(c(1,0,0,0,0), records, replace = T)
+race___05 <- sample(c(1,0,0,0,0), records, replace = T)
+race___06 <- sample(c(1,0,0,0,0), records, replace = T)
+race___07 <- sample(c(1,0,0,0,0), records, replace = T)
+
 # Create matching ages and DOBs with randomly erased DOB and age
 trues_per_false <- 15
 preserve_age <- sample(c(rep(T,trues_per_false),F), records, replace = T)
@@ -26,8 +33,8 @@ age_at_enrollment_in_years <- case_when(preserve_age & preserve_both ~ round(age
 date_of_birth <- case_when(preserve_dob & preserve_both ~ (onstudydate - age_at_enrollment_in_days), TRUE ~ as.Date(NA))
 
 # Combine my columns and write a test dataset
-test_data <- tibble(record_id, onstudydate, gender, race, ethnicity, date_of_birth, age = age_at_enrollment_in_years)
+test_data <- tibble(record_id, onstudydate, gender, race___01, race___03, race___04, race___05, race___06, race___07, ethnicity, dob = date_of_birth, age_at_enrollment = age_at_enrollment_in_years)
 write_csv(test_data, "summary_accrual_test_data.csv", na = "")
 
 # create dataset that excludes coded variables
-test_data %>% mutate(race = NA, ethnicity = NA) %>% write_csv(., "summary_accrual_test_data_imagine.csv", na = "")
+test_data %>% select(-ethnicity, -contains("race")) %>% write_csv(., "summary_accrual_test_data_imagine.csv", na = "")
