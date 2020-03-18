e5ce66f0532f85e3d10d5076732bdccecd7d8090
Kyle Chesney <kyle.2493@gmail.com>
2020-03-18 11:36:00 -0400

    enable automated sending of summary accrual data update coded variable name change

---
+++ plugins/summary_accrual_configure.php
@@ -0,0 +1,9 @@
+<?php
+
+$module->createSAConfig();
+print_r("This feature is pre-alpha");
+print_r("</br>");
+print_r("Placeholder config entry made.");
+print_r("</br>");
+print_r("Please click your browser's back button");
+// TODO: consider if an entity form is appropriate over a custom page
