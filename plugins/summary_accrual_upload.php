ed3c859a878f0428abbc5e767a452b3eed121d9e
Kyle Chesney <kyle.2493@gmail.com>
2020-05-13 16:08:03 -0400

    add modal table of record rejects and reason record ID in table links to DataEntry page for record

--- plugins/summary_accrual_upload.php
+++ plugins/summary_accrual_upload.php
@@ -5,4 +5,5 @@ require_once dirname(__DIR__) . '/classes/entity/list/SummaryAccrualList.php';
 use OnCoreClient\Entity\SummaryAccrualList;
 
 $list = new SummaryAccrualList('oncore_summary_accrual', $module);
-$list->render('project', 'Summary Accruals');
+$list->setCols(['configuration', 'updated'])
+    ->render('project', 'Summary Accruals');
